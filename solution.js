4b1fb9a00df7cfb1045370d03216c0a8a5787abf
Cem <cemkgrbz@gmail.com>
2022-07-06 15:11:29 +0200

    modified js file

---
+++ solution.js
@@ -0,0 +1,26 @@
+//1
+//Q1 - 2.==
+console.log(3 == "3");
+console.log (`3` == `"3"`);
+
+//Q2 - 7. ===
+console.log (`3` === `"3"`);
+
+//Q3 - 9. =
+
+const value = 3;
+
+//2
+
+let myVar = true;
+
+console.log(myVar ? 'good morning' : 'good evening' );
+
+//3
+
+let firstName = '' || 'John' || 0
+let emptyStr  = '' && false && 'Hello World'
+let zero  = '' || false || 0
+let seven = 75 && 'nine' && 7
+
+console.log(firstName,zero,emptyStr,seven);
